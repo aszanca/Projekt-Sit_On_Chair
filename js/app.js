f3892ff8472802dd1eb7536d4984484c784019c9
aszanca <agnieszka.pawlowska@projectplace.pl>
2017-08-05 09:59:41 +0200

    poprawki kosmetyczne

--- js/app.js
+++ js/app.js
@@ -1,107 +1,90 @@
  document.addEventListener("DOMContentLoaded", function() {
 
 
-// Dropdown menu START
+   // Dropdown menu START
 
-var main_menu = document.querySelector(".main_menu").children;
+   var main_menu = document.querySelector(".main_menu").children;
 
-    for (var i = 0; i < main_menu.length; i++) {
-      main_menu[i].addEventListener("mouseover", function() {
-        if (this.children.length > 0) {
-          this.children[1].style.display = "flex";
+   for (var i = 0; i < main_menu.length; i++) {
+     main_menu[i].addEventListener("mouseover", function() {
+       if (this.children.length > 0) {
+         this.children[1].style.display = "flex";
+       }
+     });
 
-}
+     main_menu[i].addEventListener("mouseout", function() {
+       if (this.children.length > 0) {
+         this.children[1].style.display = "none";
+       }
+     });
+   }
 
-      });
+   // Dropdown menu END
 
-      main_menu[i].addEventListener("mouseout", function() {
-if (this.children.length > 0) {
-        this.children[1].style.display = "none";
-      }
-      });
-}
+   // Slider START
 
-// Dropdown menu END
+   var prev = document.getElementById('prev');
+   var next = document.getElementById('next');
+   var slides = document.querySelectorAll('.slider_element');
+   var index = 0;
 
-// Slider START
-    var prev = document.getElementById('prev');
-    var next = document.getElementById('next');
-    var slides = document.querySelectorAll('.slider_element');
-    var index = 0;
+   slides[index].classList.add('visible');
 
-    slides[index].classList.add('visible');
+   next.addEventListener("click", function() {
+     slides[index].classList.remove('visible');
+     index++;
+     if (index == slides.length) {
+       index = 0;
+     }
+     slides[index].classList.add('visible');
+   });
 
-    next.addEventListener("click", function() {
+   prev.addEventListener("click", function() {
+     slides[index].classList.remove('visible');
+     index--;
+     if (index < 0) {
+       index = slides.length - 1;
+     }
+     slides[index].classList.add('visible');
+   });
 
-      slides[index].classList.remove('visible');
-      index++;
-      if (index == slides.length) {
-        index = 0;
-      }
-      slides[index].classList.add('visible');
-    });
+   //Slider END
 
-    prev.addEventListener("click", function() {
+   // Product boxes names hidden START
 
-      slides[index].classList.remove('visible');
-      index--;
-      if (index < 0) {
-        index = slides.length - 1;
-      }
-      slides[index].classList.add('visible');
-    });
-//Slider END
+   var product_box = document.querySelectorAll('.product_box');
 
-// Product boxes names hidden START
+   for (var i = 0; i < product_box.length; i++) {
+     product_box[i].addEventListener("mouseout", function() {
+       this.firstElementChild.style.display = "block";
+     });
+   }
 
-var product_box = document.querySelectorAll('.product_box');
+   for (var i = 0; i < product_box.length; i++) {
+     product_box[i].addEventListener("mouseover", function() {
+       this.firstElementChild.style.display = "none";
+     });
+   }
 
+   // Product boxes names hidden END
 
-for(var i = 0; i < product_box.length; i++){
-  product_box[i].addEventListener("mouseout", function(){
-this.firstElementChild.style.display = "block";
-  });
-}
+   // Pricing boxes color changes - hover START
 
+   var pricing_box = document.querySelectorAll('.pricing_box_container');
 
-for(var i = 0; i < product_box.length; i++){
-  product_box[i].addEventListener("mouseover", function(){
-    this.firstElementChild.style.display = "none";
-  });
-}
-// Product boxes names hidden END
+   for (var i = 0; i < pricing_box.length; i++) {
+     pricing_box[i].addEventListener("mouseover", function() {
+       this.parentElement.classList.add('pricing_box_hover');
+     });
+   }
 
+   for (var i = 0; i < pricing_box.length; i++) {
+     pricing_box[i].addEventListener("mouseout", function() {
+       this.parentElement.classList.remove('pricing_box_hover');
+     });
+   }
 
-// Pricing boxes color changes - hover START
-
-var pricing_box = document.querySelectorAll('.pricing_box_container');
-
-for(var i = 0; i < pricing_box.length; i++){
-  pricing_box[i].addEventListener("mouseover", function(){
-this.parentElement.classList.add('pricing_box_hover');
-
-  });
-}
-
-
-for(var i = 0; i < pricing_box.length; i++){
-  pricing_box[i].addEventListener("mouseout", function(){
-    this.parentElement.classList.remove('pricing_box_hover');
-  });
-}
+   // Pricing boxes color changes - hover END
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+ });
