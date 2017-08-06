45a74f374b6f1e01fca8b3fd1e7e66f0228ef975
aszanca <agnieszka.pawlowska@projectplace.pl>
2017-08-06 12:18:41 +0200

    zrobiony layout do sekcji calculator

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 
    // Dropdown menu START
-
    var main_menu = document.querySelector(".main_menu").children;
 
    for (var i = 0; i < main_menu.length; i++) {
@@ -18,11 +17,9 @@
        }
      });
    }
-
    // Dropdown menu END
 
    // Slider START
-
    var prev = document.getElementById('prev');
    var next = document.getElementById('next');
    var slides = document.querySelectorAll('.slider_element');
@@ -47,11 +44,9 @@
      }
      slides[index].classList.add('visible');
    });
-
    //Slider END
 
    // Product boxes names hidden START
-
    var product_box = document.querySelectorAll('.product_box');
 
    for (var i = 0; i < product_box.length; i++) {
@@ -65,11 +60,9 @@
        this.firstElementChild.style.display = "none";
      });
    }
-
    // Product boxes names hidden END
 
    // Pricing boxes color changes - hover START
-
    var pricing_box = document.querySelectorAll('.pricing_box_container');
 
    for (var i = 0; i < pricing_box.length; i++) {
@@ -83,7 +76,6 @@
        this.parentElement.classList.remove('pricing_box_hover');
      });
    }
-
    // Pricing boxes color changes - hover END
 
 
